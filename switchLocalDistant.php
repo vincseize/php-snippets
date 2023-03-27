515d638ebeb487a072171acbd998721da64bee78
vincseize <vincseize@gmail.com>
2023-03-27 12:43:49 -0400

    Update switchLocalDistant.php

--- switchLocalDistant.php
+++ switchLocalDistant.php
@@ -1,7 +1,7 @@
 <?php
 
 if( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) { // localhost ('127.0.0.1' IP in IPv4 and IPv6 formats)
-    !defined('MY_ROOT') && define('MY_LOG', 'myrootLog');
+    !defined('MY_LOG') && define('MY_LOG', 'myrootLog');
     !defined('MY_PWD') && define('MY_PWD', '');
     !defined('MY_DB') && define('MY_DB', 'lem');
     !defined('MY_IP') && define('MY_IP', 'localhost');
@@ -10,7 +10,7 @@ if( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) { // loca
 }
 
 if( !in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) { // remote 
-    !defined('MY_ROOT') && define('MY_LOG', 'myrootLog');
+    !defined('MY_LOG') && define('MY_LOG', 'myrootLog');
     !defined('MY_PWD') && define('MY_PWD', 'xxxx');
     !defined('MY_DB') && define('MY_DB', 'mydb');
     !defined('MY_IP') && define('MY_IP', 'johndoe.com');
@@ -20,9 +20,11 @@ if( !in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) { // rem
 
 try
 {
-    $dbh = new PDO("mysql:host=$ip;dbname=$dbname;charset=utf8", MY_ROOT, MY_PWD);
+    $dbh = new PDO("mysql:host=$ip;dbname=$dbname;charset=utf8", MY_ROOT, MY_LOG);
 }
 catch (PDOException $e)
 {
     echo "Error...";
 }
+
+?>
