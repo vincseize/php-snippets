1b621d39c9ae49ed725a61bbe0b7877fcf7a8c1c
vincseize <vincseize@gmail.com>
2023-03-30 10:50:03 -0400

    Create foreignKeys.php

---
+++ foreignKeys.php
@@ -0,0 +1,9 @@
+###SQL
+
+- get FK from a table
+<code>
+SELECT *
+FROM information_schema.TABLE_CONSTRAINTS
+WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+AND information_schema.TABLE_CONSTRAINTS.TABLE_NAME = 'cheeses';
+</code>
