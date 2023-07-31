8e39953c16674fccc9eda134edee0e9cc1d5694a
vincseize <vincseize@gmail.com>
2023-07-30 21:06:27 -0400

    Update sortArray_ByKeyName.php

--- sortArray_ByKeyName.php
+++ sortArray_ByKeyName.php
@@ -7,10 +7,18 @@ function sortArray_ByKeyName($keyName,$order, $a, $b) {
     } 
 }
 
-// $cats = Array ( [0] => Array ( [icon] => cat-neutre.jpg [categorie] => Anecdotes ) [1] => Array ( [icon] => histoire.png [categorie] => Histoire ) [2] => Array ( [icon] => histoires.jpg [categorie] => Bretagne ) [3] => Array ( [icon] => cat-neutre.jpg [categorie] => Tranches de Vies ) )
-          
+// $cats = Array ( 
+//            [0] => Array ( [icon] => cat-neutre.jpg [categorie] => Anecdotes ) 
+//            [1] => Array ( [icon] => histoire.png [categorie] => Histoire ) 
+//            [2] => Array ( [icon] => histoires.jpg [categorie] => Bretagne ) [3] => Array ( [icon] => cat-neutre.jpg [categorie] => Tranches de Vies ) 
+//            )
+
+print_r($cats);
+            
 $keyName = "categorie"; 
 $order = "ASC"; 
 usort($cats, function ($a, $b) use ($keyName,$order) {
     return sortArray_ByKeyName($keyName, $order, $a, $b);
 });
+
+print_r($cats);
